2272e2add422558a6366cfc1c60d3cb42451f9d8
Megatronhehe <satyamahendra09@gmail.com>
2024-01-20 17:40:17 +0700

    [feature] hero component

---
+++ src/components/Customer/Hero.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+	PiPlusBold,
+	PiFunnel,
+	PiArrowsClockwise,
+	PiPrinter,
+	PiCircle,
+} from "react-icons/pi";
+
+export default function Hero() {
+	return (
+		<>
+			<div className="bg-blue-500 rounded-xl text-white p-4 text-sm flex flex-col gap-3">
+				{/* text and description */}
+				<h2 className="text-xl">Customer</h2>
+
+				<p>On this menu you will be able to create, edit, and also delete</p>
+				<p>the customer. Also you can manage it easily.</p>
+
+				{/* interactivity */}
+				<div className="flex text-base mt-2 gap-4">
+					<button className="flex items-center gap-3 p-3 px-4 bg-opacity-30 rounded-xl bg-white">
+						<PiPlusBold />
+						Add New Customer
+					</button>
+
+					<form className="bg-white rounded-xl flex items-center justify-end flex-grow p-1 gap-1">
+						<input
+							type="text"
+							className="w-full px-4 h-full p-3 rounded-lg placeholder-gray-300"
+							placeholder="O        Search Customer"
+						/>
+						<button className=" bg-blue-500 px-4 h-full  rounded-lg">
+							Search
+						</button>
+					</form>
+
+					<button className="flex items-center gap-3 p-3 px-4 bg-opacity-30 rounded-xl bg-white">
+						<PiFunnel className="text-2xl" />
+						Filter
+					</button>
+
+					<button className="flex items-center gap-3 p-3 px-4 bg-opacity-30 rounded-xl bg-white">
+						<PiArrowsClockwise className="text-2xl" />
+						Refresh
+					</button>
+
+					<button className="gap-3 p-4 text-xl bg-opacity-30 rounded-xl aspect-square bg-white">
+						<PiPrinter />
+					</button>
+				</div>
+			</div>
+		</>
+	);
+}
--- src/pages/Customer.jsx
+++ src/pages/Customer.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Header from "../components/Customer/Header";
+import Hero from "../components/Customer/Hero";
 
 export default function Customer() {
 	return (
 		<div className="p-6 w-full">
 			<Header />
 			<main className="mt-6 flex w-full gap-6">
+				{/* hero */}
 				<div className="w-5/6">
-					<div className="bg-blue-500 rounded-xl text-white p-4">
-						<h2 className="text-xl">Customer</h2>
-					</div>
+					<Hero />
 				</div>
 				<div className="w-1/6">
 					<div>a</div>
